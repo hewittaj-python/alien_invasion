a2fb024cec74bd4ee234c5c3ad5689098ff97d22
Alex Hewitt <hewittaj@outlook.com>
2022-12-13 11:42:53 -0600

    add functionality for bullets and aliens to collide

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,12 @@ class AlienInvasion:
 
         self._create_fleet()
 
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
@@ -42,6 +48,13 @@ class AlienInvasion:
 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -90,7 +103,7 @@ class AlienInvasion:
         # Create the full fleet of aliens.
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
-                self._create_alien(alien_number)
+                self._create_alien(alien_number, row_number)
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
@@ -104,7 +117,16 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
+
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge, 
+        then update theh positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
@@ -115,6 +137,16 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
