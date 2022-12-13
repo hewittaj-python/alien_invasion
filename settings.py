5bf4f341085a9c90e8d68f4e807e37ff44f40242
Alex Hewitt <hewittaj@outlook.com>
2022-12-13 12:37:55 -0600

    add dynamic settings to the game

--- settings.py
+++ settings.py
@@ -2,18 +2,14 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Alien Settings
-        self.alien_speed = 1.0
-        # Fleet direction of 1 represents right, -1 represents left.
-        self.fleet_direction = 1
         self.fleet_drop_speed = 20
 
         # Bullet Settings
         self.bullets_allowed = 3
         self.bullet_color = (60, 60, 60)
         self.bullet_height = 15
-        self.bullet_speed = 10.0
         self.bullet_width = 3
 
         # Screen settings
@@ -23,4 +19,17 @@ class Settings:
         
         # Ship settings
         self.ship_limit = 3
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        # Fleet direction of 1 represents right, -1 represents left.
+        self.alien_speed = 1.0
+        self.bullet_speed = 10
+        self.fleet_direction = 1
         self.ship_speed = 4.5
+        
