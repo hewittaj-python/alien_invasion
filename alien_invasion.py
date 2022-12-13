a7b601fd1bc168aea0b4486afeac5e2c538037b0
Alex Hewitt <hewittaj@outlook.com>
2022-12-13 10:43:44 -0600

    add limit to bullets on screen, remove print statement

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,8 +59,9 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -73,7 +74,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
             self._update_screen()
 
     def _update_screen(self):
