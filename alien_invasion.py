e51732d00f9bdb9deb995210f8b87fc4ff797f45
Alex Hewitt <hewittaj@outlook.com>
2022-12-13 14:44:09 -0600

    add ability for aliens to be worth more in harder rounds

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,6 +62,11 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True
         )
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -114,6 +119,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
