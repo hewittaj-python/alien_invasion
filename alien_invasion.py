6f67ea8e6f36c2c239a13d920c6e39ca533d2968
Alex Hewitt <hewittaj@outlook.com>
2022-12-13 12:42:07 -0600

    add reset of game speed once play button is redisplayed

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,6 +64,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -108,6 +109,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Reset the game statistics.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
--- settings.py
+++ settings.py
@@ -25,6 +25,13 @@ class Settings:
 
         self.initialize_dynamic_settings()
 
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.alien_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.ship_speed *= self.speedup_scale
+
+        
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
         # Fleet direction of 1 represents right, -1 represents left.
